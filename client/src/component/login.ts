07f2d2ff706feec85149ce0847bb2c81ef985838
Amit Ashtekar <amit.ashtekar@synerzip.com>
2016-04-15 15:15:58 +0530

    Update modules

--- client/src/component/login.ts
+++ client/src/component/login.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from 'angular2/core';;
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from 'angular2/core';
 import {Observable} from  'rxjs/Observable';
 import {Store} from '@ngrx/store';
 import {loginStore} from '../interface'
